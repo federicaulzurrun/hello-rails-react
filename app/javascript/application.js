058f4c48ff5904322085102056197ff51d5bcb21
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-09-04 18:38:15 -0300

    fixed import App on application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import App from './components/app';
+import App from './components/App';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
